f94a9df43a53eecc0df8b1facf0d08cec4e4143d
GaganTut <gstut@live.com>
2017-02-17 21:01:29 -1000

    Finished Number 4/5

--- js/app.js
+++ js/app.js
@@ -21,11 +21,12 @@ alias3.innerHTML = "Concatenation";
 
 /*4. Replace the n/a for Snoop with the gangsta lorem ipsum*/
 
+var snoopProfile = document.getElementsByClassName("profile");
 
+snoopProfile[0].innerHTML = "Lorem ipsizzle gangster pot bling bling, sheezy adipiscing elit. Nullizzle shiz velizzle, dawg volutpizzle, suscipizzle uhuh ... yih!, gravida vel, gangster. Pellentesque eget tortizzle. Gangsta erizzle. Pimpin' izzle go to hizzle dapibus turpis tempus tempor. Mauris the bizzle nibh et turpis. Fo shizzle in tortor. Pellentesque boofron rhoncizzle nisi. In hizzle habitasse platea dictumst. Sizzle dapibus. Curabitur tellus crazy, pretizzle crazy, mattizzle pot, eleifend vitae, nunc. We gonna chung suscipit. Integizzle sempizzle sheezy sizzle shizzlin dizzle.";
 
 /*5. Relace the n/a for Samuel L Jackson with his lorem ipsum*/
-
-
+document.getElementsByClassName("profile")[2].innerHTML = "Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? No? Well, that's what you see at a toy store. And you must think you're in a toy store, because you're here shopping for an infant named Jeb.";
 
 
 /*6. Create a div element for Chuck Norris and give it an id of "name7". Inside this div element, give it the contents of "Chuck Norris".
